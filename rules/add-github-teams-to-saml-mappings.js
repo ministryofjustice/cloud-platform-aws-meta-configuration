85642611ba5b77c6cf6ef34511816f0e49b44022
Todd Tyree <tatyree@gmail.com>
2018-06-26 13:20:04 +0100

    Change saml team mapping to only use github-webops
    This is the only team->role mapping we need right now.  I have also
    changed the hardcoded ARNs to point at the correct info on the correct
    account.

--- rules/add-github-teams-to-saml-mappings.js
+++ rules/add-github-teams-to-saml-mappings.js
@@ -4,11 +4,9 @@
 function (user, context, callback) {
   var request = require('request');
 
-  // Apply to 'github' connections only
   if(context.connection === 'github'){
     var github_identity = _.find(user.identities, { connection: 'github' });
 
-    // Get user's Github team list
     var teams_req = {
       url: 'https://api.github.com/user/teams',
       headers: {
@@ -22,23 +20,15 @@ function (user, context, callback) {
         return callback(new Error('Error retrieving teams from github: ' + body || err));
       }
 
-      // IAM resource constants
-      // `idp_arn` - reference to the SAML provider that has a trust relationship with AWS
-      var idp_arn = "arn:aws:iam::926803513772:saml-provider/shared-auth0";
-      var role_base_arn = "arn:aws:iam::926803513772:role/";
+      // This can be found in the AWS Console -> Providers -> <Specific Provider> -> Provider ARN
+      var idp_arn = "arn:aws:iam::754256621582:saml-provider/moj-cloud-platform-aws";
+      // This can be found in the AWS Console -> Roles -> <Specific Role> -> *Base* of Role ARN
+      var role_base_arn = "arn:aws:iam::754256621582:role/";
 
-      // Add list of IAM roles that the user can assume, one role per Github team
-      // SAML spec requires that the IDP identifier is included with each role
-      // identifier, separated with a comma
-      user.awsRole = JSON.parse(body).map(function (team) {
-        return role_base_arn + "test-github-" + team.slug + "," + idp_arn;
-      });
-
-      // Name for the user's login session, typically their username
-      // The AWS console will display the logged-in account as `role_name/session_name`,
-      // e.g. `github_webops/kerin`
+      // For now, this is the only role available.
+      user.awsRole = ["github-webobs"];
       user.awsRoleSession = user.nickname;
-    
+
       // Map the user's AWS roles and session name to the equivalent SAML attributes
       context.samlConfiguration.mappings = {
         'https://aws.amazon.com/SAML/Attributes/Role': 'awsRole',

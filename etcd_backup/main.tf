7842c9f58b2536f95cc864c9400b9990b34b9151
Sablu Miah <sablu.miah@digital.justice.gov.uk>
2018-11-13 11:14:06 +0000

    change bucket name

--- etcd_backup/main.tf
+++ etcd_backup/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cloud-platform-intergration-etcdbackup-terraform-state"
+    bucket = "cloud-platform-<aws_account>-etcdbackup-terraform-state"
     region = "eu-west-1"
     key    = "terraform.tfstate"
   }

ee4535f6aa532ca102f45714e29230017ec299c8
Razvan Cosma <36888774+razvan-moj@users.noreply.github.com>
2018-11-22 10:03:49 +0000

    3 instances for audit (#19)
    * 3 instances for audit
    
    * test off

--- Kibana/main.tf
+++ Kibana/main.tf
@@ -23,55 +23,6 @@ data "terraform_remote_state" "global" {
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
-resource "aws_elasticsearch_domain" "test" {
-  domain_name           = "${local.test_domain}"
-  elasticsearch_version = "6.2"
-
-  cluster_config {
-    instance_type  = "m4.large.elasticsearch"
-    instance_count = "3"
-  }
-
-  ebs_options {
-    ebs_enabled = "true"
-    volume_type = "gp2"
-    volume_size = "150"
-  }
-
-  advanced_options {
-    "rest.action.multi.allow_explicit_index" = "true"
-  }
-
-  access_policies = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "es:*",
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.test_domain}/*",
-      "Condition": {
-        "IpAddress": {
-          "aws:SourceIp": ${jsonencode(keys(local.allowed_test_ips))}
-        }
-      }
-    }
-  ]
-}
-CONFIG
-
-  snapshot_options {
-    automated_snapshot_start_hour = 23
-  }
-
-  tags {
-    Domain = "${local.test_domain}"
-  }
-}
-
 resource "aws_elasticsearch_domain" "live" {
   domain_name           = "${local.live_domain}"
   elasticsearch_version = "6.2"
@@ -127,7 +78,7 @@ resource "aws_elasticsearch_domain" "audit" {
 
   cluster_config {
     instance_type  = "m4.large.elasticsearch"
-    instance_count = "2"
+    instance_count = "3"
   }
 
   ebs_options {
